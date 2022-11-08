d213f39870a4f50e187813b24670823a181974f9
Anushka Kishor <111566100+kishoranushka@users.noreply.github.com>
2022-11-08 11:50:49 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,10 @@
+//wap in java to print the elements of array
+
+class array {
+    public static void main(String a[])
+    {
+        int x[]={10,20,30,40};
+        System.out.println(x[0] + " " + x[1] + " " + x[2] + " " + x[3]);
+    }
+    
+}
